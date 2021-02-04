6b7f99f5648839401878bd38460ab5ea4cc63e43
Peter.Pan <defu_pan@163.com>
2021-02-04 10:40:51 +0800

    第一次测试提交

---
+++ DataStructureUnitTest/Bu/DataStructureUnitTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureUnitTest
+{
+
+    public partial class DataStructureUnitTest
+    {
+    }
+}
---
+++ DataStructureUnitTest/Peter_Pan/DataStructureUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureUnitTest
+{
+    [TestClass]
+    public partial class DataStructureUnitTest
+    {
+        [TestMethod]
+        public string TestMethod1()
+        {
+        }
+    }
+}
