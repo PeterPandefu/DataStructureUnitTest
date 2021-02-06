ed5e774d87407b7e8c2aaf7173282e32d5e20ac7
Peter.pan <defu_pan@163.com>
2021-02-06 20:21:39 +0800

    新增LeetCode算题1

--- DataStructureUnitTest/Peter.Pan/LeetCodeAlgorithmProblem.cs
+++ DataStructureUnitTest/Peter.Pan/LeetCodeAlgorithmProblem.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace DataStructureUnitTest.Peter.Pan
-{    
+{
     /// <summary>
     /// LeetCode的算法题单元测试
     /// </summary>
-    public  class LeetCodeAlgorithmProblem
+    [TestClass]
+    public class LeetCodeAlgorithmProblem
     {
+        [ClassInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+
+        }
+        #region 1 两数之和
+        //给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+        //你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+        //你可以按任意顺序返回答案。
+        [TestMethod]
+        [DataRow(99, new int[10] { 12, 24, 90, 73, 84, 56, 48, 27, 19, 99 })]
+        public void LeetCode_Method_1(int expected, int[] arr)
+        {
+
+        }
+        #endregion
+
     }
 }
