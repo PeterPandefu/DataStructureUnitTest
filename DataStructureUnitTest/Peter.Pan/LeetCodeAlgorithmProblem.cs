a6f6fccd8dab75dec510900fc0cf0650b8c45d52
Peter.Pan <defu_pan@163.com>
2021-02-07 16:28:56 +0800

    test 新增测试 No2 未完成测试

--- DataStructureUnitTest/Peter.Pan/LeetCodeAlgorithmProblem.cs
+++ DataStructureUnitTest/Peter.Pan/LeetCodeAlgorithmProblem.cs
@@ -52,7 +52,8 @@ namespace DataStructureUnitTest.Peter.Pan
                 }
             }
 
-            //两个循环 
+            //两个循环  每次核心代码的执行次数为  n+(n-1)+(n-2)+.....+1=n(n-1)/2
+            //所以时间复杂度为O(n^2)
         }
 
         public void LeetCode_No1_Method2(int expected, int[] arr, out int index1, out int index2)
@@ -63,16 +64,54 @@ namespace DataStructureUnitTest.Peter.Pan
             Dictionary<int, int> dic = new Dictionary<int, int>();
             for (int i = 0; i < arr.Length; i++)
             {
-                if (dic.ContainsKey(expected- arr[i]))
+                if (dic.ContainsKey(expected - arr[i]))
                 {
                     index1 = dic[expected - arr[i]];
                     index2 = i;
                 }
                 dic.Add(arr[i], i);
             }
+            //一个循环  所以执行次数为n 时间复杂度则为O(n)
         }
 
         #endregion
 
+        #region 2 两数相加
+        //给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+        //请你将两个数相加，并以相同形式返回一个表示和的链表。
+        //你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+        [TestMethod]
+        [TestCategory("未已完成的测试")]
+        [DataRow(708, new int[] { 2, 4, 3 }, new int[] { 5, 6, 4 })]
+        [DataRow(0, new int[] { 0 }, new int[] { 0 })]
+        [DataRow(89990001, new int[] { 9, 9, 9, 9, 9, 9, 9 }, new int[] { 9, 9, 9, 9 })]
+        public void LeetCode_No2(int expected, int[] arr1, int[] arr2)
+        {
+
+            LinkedList<int> linkedListA = new LinkedList<int>(arr1);
+            LinkedList<int> linkedListB = new LinkedList<int>(arr2);
+            //因为是逆序存放 所以计算是直接从第一个开始取值 如果顺序存放 则需要逆序取值
+            LinkedListNode<int> elementA = linkedListA.First;
+            LinkedListNode<int> elementB = linkedListB.First;
+
+            //是否需要进位
+            bool carry = false;
+
+            int v1 = 0;
+            int v2 = 0;
+
+
+            while (elementA != null && elementA != null)
+            {
+                //v1= elementA.Value
+            }
+
+
+        }
+
+
+
+
+        #endregion
     }
 }
