2ef6a18035550984366e7e127839f85a135bcf9e
Peter.pan <defu_pan@163.com>
2021-02-07 21:52:21 +0800

    test 新增单元测试 No2 两数相加

--- DataStructureUnitTest/Peter.Pan/LeetCodeAlgorithmProblem.cs
+++ DataStructureUnitTest/Peter.Pan/LeetCodeAlgorithmProblem.cs
@@ -78,34 +78,108 @@ namespace DataStructureUnitTest.Peter.Pan
 
         #region 2 两数相加
         //给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
-        //请你将两个数相加，并以相同形式返回一个表示和的链表。
+        //请你将两个数相加，并以相同形式返回一个表示和的链表。                                    
         //你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+        //输入：l1 = [2,4,3], l2 = [5,6,4]
+        //输出：[7,0,8]
+        //解释：342 + 465 = 807.
+
+        //示例 2：
+        //输入：l1 = [0], l2 = [0]
+        //输出：[0]
+
+        //示例 3：
+        //输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+        //输出：[8,9,9,9,0,0,0,1]
+        //解释: 9999000+9999999=10009998
+
+
         [TestMethod]
-        [TestCategory("未已完成的测试")]
-        [DataRow(708, new int[] { 2, 4, 3 }, new int[] { 5, 6, 4 })]
+        [TestCategory("已完成的测试")]
+        [DataRow(807, new int[] { 2, 4, 3 }, new int[] { 5, 6, 4 })]
         [DataRow(0, new int[] { 0 }, new int[] { 0 })]
-        [DataRow(89990001, new int[] { 9, 9, 9, 9, 9, 9, 9 }, new int[] { 9, 9, 9, 9 })]
+        [DataRow(10009998, new int[] { 9, 9, 9, 9, 9, 9, 9 }, new int[] { 9, 9, 9, 9 })]
         public void LeetCode_No2(int expected, int[] arr1, int[] arr2)
         {
 
             LinkedList<int> linkedListA = new LinkedList<int>(arr1);
             LinkedList<int> linkedListB = new LinkedList<int>(arr2);
-            //因为是逆序存放 所以计算是直接从第一个开始取值 如果顺序存放 则需要逆序取值
+
+
             LinkedListNode<int> elementA = linkedListA.First;
             LinkedListNode<int> elementB = linkedListB.First;
 
-            //是否需要进位
-            bool carry = false;
+            //获取两个链表的长度
+            int linkedListALength = 0;
+            while (elementA != null)
+            {
+                elementA = elementA.Next;
+                linkedListALength++;
+            }
+
+            int linkedListBLength = 0;
+            while (elementB != null)
+            {
+                elementB = elementB.Next;
+                linkedListBLength++;
+            }
+            elementA = linkedListA.First;
+            elementB = linkedListB.First;
+
+            //填充短链表  填0
+            if (linkedListALength > linkedListBLength)
+            {
+                for (int i = 0; i < linkedListALength - linkedListBLength; i++)
+                {
+                    elementB.List.AddLast(new LinkedListNode<int>(0));
+                }
+            }
+            if (linkedListALength < linkedListBLength)
+            {
+                for (int i = 0; i < linkedListBLength - linkedListALength; i++)
+                {
+                    elementA.List.AddLast(new LinkedListNode<int>(0));
+                }
+            }
+
+            //接着让  elementA elementB指向链表的第一个元素
+            elementA = linkedListA.First;
+            elementB = linkedListB.First;
+
+            //得到的结果
+            List<int> resList = new List<int>();
+            int res = 0;
 
             int v1 = 0;
             int v2 = 0;
 
+            //是否需要进位
+            int carry = 0;
 
-            while (elementA != null && elementA != null)
+            //循环次数
+            int cycles = linkedListALength > linkedListBLength ? linkedListALength : linkedListBLength;
+            for (int i = 0; i < cycles; i++)
             {
-                //v1= elementA.Value
-            }
+                v1 = elementA.Value;
+                v2 = elementB.Value;
+                int num = (v1 + v2 + carry) % 10;
+                carry = (v1 + v2 + carry) / 10;
+                resList.Add(num);
+                elementA = elementA.Next;
+                elementB = elementB.Next;
 
+                //若最后一位有进位 则再新增一个
+                if (i == cycles - 1 && carry == 1)
+                {
+                    resList.Add(1);
+                }
+            }
+            //将结果倒叙算值
+            resList.Reverse();
+            if (resList.Count > 0) resList.ForEach(t => { res = res * 10 + t; });
+
+            Assert.AreEqual(expected, res);
 
         }
 
